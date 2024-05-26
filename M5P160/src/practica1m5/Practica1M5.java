36935b4c709d090ba17f72272022ff9c6c2a3493
sebas <sebas@192.168.1.22>
2024-05-26 11:25:37 +0200

    Añadimos todas las clases incluyendo el diagrama de ellas

---
+++ M5P160/src/practica1m5/Practica1M5.java
@@ -0,0 +1,16 @@
+package practica1m5;
+
+/**
+ *
+ * @author sebas
+ */
+public class Practica1M5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
